e62985b05e7ba7c99f4d3d422821333bf181681c
Iulian Preda <iulian.octavian.preda@gmail.com>
2022-03-03 13:48:31 +0100

    Update benchmark.ts

--- packages/benchmark/src/lib/benchmark.ts
+++ packages/benchmark/src/lib/benchmark.ts
@@ -1,3 +1,16 @@
-export function benchmark(): string {
-    return 'benchmark';
+export class Benchmark {
+    constructor(options: any) {}
+
+    add(name: string, fn: Function) {
+        console.log(name, fn);
+        return this;
+    }
+
+    run() {
+        return this;
+    }
+}
+
+export function createBenchmark(options: any) {
+    return new Benchmark(options);
 }
