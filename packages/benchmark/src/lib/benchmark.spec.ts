92bb73209c502f1470411dba7d9f1c0db4930653
Gabriel Preda <gabriel.andrei.preda@gmail.com>
2022-03-04 17:06:42 +0200

    Base code

--- packages/benchmark/src/lib/benchmark.spec.ts
+++ packages/benchmark/src/lib/benchmark.spec.ts
@@ -1,7 +1,30 @@
-import { benchmark } from './benchmark';
+import { Benchmark } from './benchmark';
 
+// import { A } from './benchmark';
 describe('benchmark', () => {
-    it('should work', () => {
-        expect(benchmark()).toEqual('benchmark');
+    // it('should work', () => {
+    //     expect(benchmark()).toEqual('benchmark');
+    // });
+
+    // it('A', () => {
+    //     // const a = new A();
+    //    console.log(a.add(1).subtract(10).add(100).showTotal());
+    //    const bench = new Benchmark();
+
+    //    bench.addSuite('A', () => {
+    //     // expect().toEqual(1);
+    //     // expect(benchmark()).toEqual('benchmark');
+    // });
+
+     it('should work', () => {
+       const bench = new Benchmark();
+       bench.add('A', (a,b) => {
+            return a**b**5000;
+       },[[14,3],[22,22],[15,5]])
+       .add('B', (a,b) => {return a*5*b},[[14,3],[22,22],[15,5]])
+       .run()
+       .statistics();
+        
     });
+
 });
--- packages/benchmark/src/lib/benchmark.ts
+++ packages/benchmark/src/lib/benchmark.ts
@@ -1,16 +1,80 @@
-export class Benchmark {
-    constructor(options: any) {}
 
-    add(name: string, fn: Function) {
-        console.log(name, fn);
+
+
+
+export class Benchmark {
+    suites:Array<{
+        name:string,
+        fn:(...args:any[])=>unknown,
+        testData:any[][],
+        times:number[]
+    }> = [];
+    // constructor(options: any) {}
+
+    add(name: string, fn: (...args:any[])=>unknown,testData:any[][]) {
+        this.suites.push({name,fn,testData,times:[]});
         return this;
     }
 
     run() {
+// testData = [[1,2,3],[4,5,6],[7,8,9]]
+        for(let i=0; i<1000; i++) {
+        this.suites.forEach(suite => {
+            suite.testData.forEach(data => {
+               suite.times.push(measureTimeMs( () => suite.fn(...data)));
+            })
+        });
+    }
+        return this;
+    }
+    
+    statistics(){
+        const averageTimesWithName =  this.suites.map(suite => {
+            const sum = suite.times.reduce((previousVal, currentVal) => {return previousVal + currentVal}, 0);
+            const avg = sum / suite.times.length;
+            return {
+                name: suite.name,
+                avgTime:avg
+            }
+        }).sort((a,b) => a.avgTime - b.avgTime);
+
+        averageTimesWithName.forEach(suite => {
+            console.log(`${suite.name}: ${suite.avgTime}ms`);
+        });
+
         return this;
     }
 }
 
-export function createBenchmark(options: any) {
-    return new Benchmark(options);
+// export function createBenchmark(options: any) {
+//     return new Benchmark(options);
+// }
+
+//  export class A {
+//     total = 0;
+//     add(a: number) {
+//         this.total += a;
+//         return this;
+//     }
+
+//     subtract(a: number) {
+//         this.total -= a;
+//         return this;
+//     }
+
+//     showTotal(){
+//         return this.total;
+//     }
+// }
+ 
+
+// import { performance as nodePerformance } from 'perf_hooks';
+
+export function measureTimeMs(
+    callback: () => unknown,
+    performance = (globalThis as any).performance 
+) {
+    const time = performance.now();
+    callback();
+    return performance.now() - time;
 }
